011d77b48cf76da3f411f412932319a4692dab77
failer64 <80625459+failer64@users.noreply.github.com>
2023-11-21 02:31:37 +0300

    Add files via upload

---
+++ dist/js/app.js
@@ -0,0 +1,11 @@
+(() => {
+    "use strict";
+    function addLoadedClass() {
+        window.addEventListener("load", (function() {
+            setTimeout((function() {
+                document.documentElement.classList.add("loaded");
+            }), 0);
+        }));
+    }
+    addLoadedClass();
+})();
